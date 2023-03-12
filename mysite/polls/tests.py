2ca53b802cb1e2f2f2d9cba5fcd276a662364675
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-03-12 18:39:46 +0100

    Unit test for question details

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -72,3 +72,17 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             [question2, question1]
         )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text='Future question.', days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_question.id,)))
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text='Past question.', days=-5)
+        response = self.client.get(reverse('polls:detail', args=(past_question.id,)))
+
+        self.assertContains(response, past_question.question_text)
