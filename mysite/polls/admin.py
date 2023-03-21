7be691d9c218ba5e3b3660f7126ce167fc018159
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-03-21 13:40:53 +0100

    Edit admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
