eba6c9f1d1e1110b183cc5461babafdc4f4a2e80
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-04-09 06:01:43 +0200

    Add csrf api to back-end. Not sure what it does exactly...

--- authentication/api/views.py
+++ authentication/api/views.py
@@ -2,9 +2,15 @@ import json
 
 from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
+from django.middleware.csrf import get_token
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_POST
 
+
+def get_csrf(request):
+    response = JsonResponse({'detail': 'CSRF cookie set'})
+    response['X-CSRFToken'] = get_token(request)
+    return response
 
 @require_POST
 def login_view(request):
