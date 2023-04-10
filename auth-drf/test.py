5611f13ed4fac0073e3edf1dc9ff66d9902301d6
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-04-10 06:39:39 +0200

    Add simple requests example for drf with token auth

---
+++ auth-drf/test.py
@@ -0,0 +1,28 @@
+import requests
+
+
+url = 'http://127.0.0.1:8000/users/'
+url_token = 'http://127.0.0.1:8000/api-auth/token/'
+
+response = requests.get(url)
+print(response)
+
+
+data = {
+    'username': 'noud',
+    'password': 'noud',
+}
+
+response = requests.post(url_token, data=data)
+print(response.status_code)
+print(response.json())
+
+token = response.json()['token']
+
+headers = {
+    'Authorization': f'Token {token}',
+}
+
+response = requests.get(url, headers=headers)
+print(response.status_code)
+print(response.json())
