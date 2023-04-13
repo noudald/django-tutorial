47c51ab6abcb8554bcb9a9582a9db51f584ce4ba
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-04-13 05:32:50 +0200

    Upgrade the router slightly... next would be a good constructor

--- auth-drf/vanilla-frontend/src/main.js
+++ auth-drf/vanilla-frontend/src/main.js
@@ -1,25 +1,61 @@
 /* Main javascript file. */
 
-fetch('/helloworld.html')
-  .then((response) => response.text())
-  .then((body) => document.querySelector('#mainContainer').innerHTML = body)
-  .catch((error) => console.log(error));
-
-
+// Router class that manages all pages.
 class Router {
   constructor(pages) {
-    // Do nothing.
+    // Do nothing (yet).
   }
 
   navigate(path) {
-    console.log(path);
+    var pages = {
+      'login': {
+        'urlContainer': '/container/login.html',
+        'title': 'Login page',
+        'urlIndex': 'login',
+        'jsFiles': ['/src/login.js'],
+      }
+    };
 
-    if (path === 'home') {
-      window.history.pushState({path}, 'Home page', 'index.html#home');
-    } else if (path == 'test') {
-      window.history.pushState({path}, 'Test page', 'index.html#test');
+    const elementHTMLContainer = 'mainContainer';
+
+    if (path in pages) {
+      const page = pages[path];
+
+      fetch(page.urlContainer)
+        .then((response) => response.text())
+        .then((body) => {
+          const container = document.getElementById(elementHTMLContainer);
+
+          container.innerHTML = body;
+
+          if ('jsFiles' in page) {
+            page.jsFiles.forEach((file) => {
+              var jsScript = document.createElement('script');
+              jsScript.type = 'text/javascript';
+              jsScript.src = file;
+              container.appendChild(jsScript);
+            });
+          }
+
+          if ('cssFiles' in page) {
+            page.cssFiles.forEach((file) => {
+              const cssLink = document.createElement('link');
+              cssLink.rel = 'stylesheet';
+              cssLink.href = file;
+              container.appendChild(cssLink);
+            });
+          }
+
+          window.history.pushState({path}, page.title, 'index.html' + window.location.search + '#' + page.urlIndex);
+        })
+        .catch((error) => console.log("Error", error));
+    } else {
+      console.log(`Error loading path ${path}`);
     }
   }
 }
 
+
 const router = new Router({});
+
+router.navigate('login');
