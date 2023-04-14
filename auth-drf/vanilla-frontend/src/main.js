774d0a7382ea80e9deeb056b51d777318a1ef8c6
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-04-14 06:12:10 +0200

    Move router to seperate file

--- auth-drf/vanilla-frontend/src/main.js
+++ auth-drf/vanilla-frontend/src/main.js
@@ -1,53 +1,6 @@
 /* Main javascript file. */
 
-// Router class that manages all pages.
-class Router {
-  constructor(pages, config) {
-    this.pages = pages;
-    this.rootURL = config['rootURL'];
-    this.elementHTMLContainer = config['elementHTMLContainer'];
-  }
-
-  navigate(pageName) {
-    if (pageName in this.pages) {
-      const page = this.pages[pageName];
-
-      fetch(page.urlContainer)
-        .then((response) => response.text())
-        .then((body) => {
-          const container = document.getElementById(this.elementHTMLContainer);
-          container.innerHTML = body;
-
-          if ('jsFiles' in page) {
-            page.jsFiles.forEach((file) => {
-              const jsScript = document.createElement('script');
-              jsScript.type = 'text/javascript';
-              jsScript.src = file;
-              container.appendChild(jsScript);
-            });
-          }
-
-          if ('cssFiles' in page) {
-            page.cssFiles.forEach((file) => {
-              const cssLink = document.createElement('link');
-              cssLink.rel = 'stylesheet';
-              cssLink.href = file;
-              container.appendChild(cssLink);
-            });
-          }
-
-          window.history.pushState(
-            {pageName},
-            page.title,
-            this.rootURL + window.location.search + '#' + page.urlIndex
-          );
-        })
-        .catch((error) => console.log("Error", error));
-    } else {
-      console.log(`Error loading page ${pageName}`);
-    }
-  }
-}
+import { Router } from './router.js';
 
 // Example how to get current values from url.
 const hashValue = window.location.hash.substring(1);
@@ -56,10 +9,6 @@ const searchParams = new URLSearchParams(searchValue);
 
 console.log(hashValue);
 console.log(searchParams);
-
-window.addEventListener('hashchange', () => {
-  console.log('Hash changed', window.location.hash);
-});
 
 
 const routerPages = {
