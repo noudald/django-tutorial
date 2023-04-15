d8ec732afe2c898f2b312580d20173d3ba02680d
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-04-15 06:23:44 +0200

    Create dynamic navigation bar

--- auth-drf/vanilla-frontend/src/main.js
+++ auth-drf/vanilla-frontend/src/main.js
@@ -12,16 +12,42 @@ console.log(hashValue);
 console.log(searchParams);
 
 
+class NavBar {
+	constructor(pages, config) {
+		this.pages = pages;
+		this.elementHTMLNavBar = config['elementHTMLNavBar'];
+
+		this.renderNavBar();
+	}
+
+	renderNavBar() {
+    const navBarElement = document.querySelector(`.${this.elementHTMLNavBar}`);
+    const navBarList = document.createElement('ul');
+
+		Object.keys(this.pages).forEach((page) => {
+      const navBarItem = document.createElement('li');
+      const navBarLink = document.createElement('a');
+
+      navBarLink.href = `#${page}`;
+      navBarLink.text = this.pages[page].title;
+
+      navBarItem.appendChild(navBarLink);
+      navBarList.appendChild(navBarItem);
+		});
+
+    navBarElement.appendChild(navBarList);
+	}
+}
+
+
 const routerPages = {
   login: {
     title: 'Login page',
-    urlIndex: 'login',
     urlContainer: '/container/login.html',
     jsFiles: ['/src/login.js'],
   },
   helloworld: {
     title: 'Hello World!',
-    urlIndex: 'helloworld',
     urlContainer: '/container/helloworld.html',
   },
 };
@@ -34,3 +60,6 @@ const routerConfig = {
 const router = new Router(routerPages, routerConfig);
 
 router.navigate('login');
+
+
+const navBar = new NavBar(routerPages, { elementHTMLNavBar: 'navbar' });
