7867fb159a4431deccd59309d4bcd791cbd2559b
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-04-16 05:32:28 +0200

    Allow CORS on localhost with vanilla-frontend

--- auth-drf/auth_drf/settings.py
+++ auth-drf/auth_drf/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,9 +138,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS settings
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8001',
-    'http://localhost:8001',
+    'http://127.0.0.1:8080',
+    'http://localhost:8080',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Django REST Framework settings
