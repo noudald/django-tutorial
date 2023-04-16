3002040329d7cd6360bf762003cc032a2ef10f65
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-04-16 05:30:18 +0200

    Add check if pages are well configured in NavBar

--- auth-drf/vanilla-frontend/src/navbar.js
+++ auth-drf/vanilla-frontend/src/navbar.js
@@ -2,11 +2,23 @@
 
 export class NavBar {
 	constructor(pages, config) {
-		this.pages = pages;
 		this.elementHTMLNavBar = config['elementHTMLNavBar'];
+
+    this.pages = {};
+    for (const [pageHash, page] of Object.entries(pages)) {
+      this.addPage(pageHash, page);
+    }
 
 		this.renderNavBar();
 	}
+
+  addPage(pageHash, page) {
+    if (!('title' in page)) {
+      throw new Error('Page should contain title');
+    }
+
+    this.pages[pageHash] = page;
+  }
 
 	renderNavBar() {
     const navBarElement = document.querySelector(`.${this.elementHTMLNavBar}`);
