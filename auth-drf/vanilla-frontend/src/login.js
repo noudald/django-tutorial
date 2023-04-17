a1c20cc178745be49602b6cfffe6315738f9cca3
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-04-17 06:59:20 +0200

    Create logout method in authenticator with whoami authentication

--- auth-drf/vanilla-frontend/src/login.js
+++ auth-drf/vanilla-frontend/src/login.js
@@ -1,10 +1,31 @@
 /* Login Javascript template */
 
+if (globalVars.authenticator.amIAuthenticated()) {
+  globalVars.authenticator.whoAmI()
+    .then((data) => {
+      const welcomeMessageElement = document.querySelector('#welcomeMessage');
+      if (data.status) {
+        welcomeMessageElement.innerHTML = `Welcome ${data.username}!`;
+      } else {
+        welcomeMessageElement.innerHTML = 'Welcome, unfortunately I could not figure out who you are.';
+      }
+    });
+
+  document.querySelector('#formLogin').style.display = 'none';
+  document.querySelector('#formLogout').style.display = 'block';
+} else {
+  document.querySelector('#formLogin').style.display = 'block';
+  document.querySelector('#formLogout').style.display = 'none';
+}
+
+
 document
   .getElementById('formLogin')
   .addEventListener('submit', (event) => {
     event.preventDefault();
     console.log('Prevent doing anything');
+
+    const loginErrorMessageElement = document.querySelector('#loginErrorMessage');
 
     const username = document.querySelector('#inputUsername');
     const password = document.querySelector('#inputPassword');
@@ -14,9 +35,12 @@ document
       .then((result) => {
         if (result.status) {
           window.location.hash = '#helloworld';
+          loginErrorMessage.innerHTML = 'Could not login';
+          globalVars.authenticator.whoAmI()
+            .then((result) => console.log(result));
+        } else {
+          loginErrorMessageElement.innerHTML = 'Could not login';
         }
-        globalVars.authenticator.whoAmI()
-          .then((result) => console.log(result));
       });
 
 
@@ -28,6 +52,22 @@ document
   .getElementById('inputLogin')
   .addEventListener('click', () => {
     console.log('click')
+  });
+
+document
+  .querySelector('#formLogout')
+  .addEventListener('submit', (event) => {
+    event.preventDefault();
+    globalVars.authenticator
+      .logOut()
+      .then((result) => {
+        if (result.status) {
+          window.location.reload();
+        } else {
+          throw new Error('Could not logout');
+        }
+      })
+      .catch((error) => alert('Could not logout user.'));
   });
 
 console.log('Login script ran');
