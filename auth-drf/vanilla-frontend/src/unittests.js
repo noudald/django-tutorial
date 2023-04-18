995fdcc00b18d8f6ab35515989cb52ed01c5c393
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-04-18 05:39:45 +0200

    Add some simple tests to check if unittest library works

--- auth-drf/vanilla-frontend/src/unittests.js
+++ auth-drf/vanilla-frontend/src/unittests.js
@@ -92,3 +92,46 @@ class UnitTests {
     });
   }
 }
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+class TestSuite extends UnitTests {
+  testSuiteSimpleTest() {
+    sleep(500);
+    this.assertTrue(1 + 1 === 3);
+  }
+
+  testSuiteSimpleTest2() {
+    sleep(500);
+    this.assertTrue(2 * 2 === 4);
+  }
+
+  testSuiteSimpleTest3() {
+    sleep(500);
+    this.assertTrue(2 * 2 === 4);
+  }
+
+  testSuiteAssertEqual() {
+    this.assertEqual(2 + 2, 4, '2 + 2 should be 4');
+    this.assertEqual(2 + 2, 5, '2 + 2 should be 5');
+  }
+
+  testServerConnection() {
+    // TODO: Doesn't work!
+    const img = document.createElement('img');
+    const server_url = 'http://localhost:8000'
+    img.onerror = () => {
+      this.raiseError(`Server server ${server_url} is offline`);
+    };
+    img.src = server_url + '/static/ping.png';
+  }
+}
+
+const testSuite = new TestSuite(['testSuite', 'testServer']);
+testSuite.runTests()
